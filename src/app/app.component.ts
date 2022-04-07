46159e1cd15ecdc04b3752043cd4f5e2dcfdb7f2
Carlos Santana Rodríguez <carlos.santana.pelusa@gmail.com>
2022-04-08 00:35:41 +0100

    CRUD done
    CRUD finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-form';
+  userArray: User[] = [
+  ];
+
+  selectedUser: User = new User();
+
+  addOrEdit() {
+    if(this.selectedUser.id === 0) {
+      this.selectedUser.id = this.userArray.length + 1;
+      this.userArray.push(this.selectedUser);
+    }
+
+    this.selectedUser = new User();
+  }
+
+  openForEdit(user: User) {
+    this.selectedUser = user;
+  }
+
+  delete() {
+    if(confirm('¿Seguro que quieres eliminarlo?')) {
+      this.userArray = this.userArray.filter (x => x != this.selectedUser);
+      this.selectedUser = new User();
+    }
+  }
+
 }
